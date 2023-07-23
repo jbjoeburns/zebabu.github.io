cf0dfdd01575313d4469e1f0e6b3966cd3eec348
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-07-24 00:30:28 +0100

    Set up react

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import "./homepage.css";
+
+const Homepage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="container">
+      test
+    </div>
+  );
+};
+
+export default Homepage;
